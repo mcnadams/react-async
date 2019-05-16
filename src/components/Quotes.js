a9358c6ab88bc99eda218e9a079f3c16b7909905
mcnadams <mcneilbo@oregonstate.edu>
2019-05-16 16:31:53 -0700

    style

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropType from 'prop-types';
 import Quote from './Quote';
+import styles from './Quotes.css';
 
 function Quotes({ quotes }) {
   const quoteLis = quotes.map((quote, i) => {
-    <li key={i}>
-      <Quote quote={quote} />
-    </li>;
+    return (
+      <li key={i}>
+        <Quote quote={quote} />
+      </li>
+    );
   });
 
   return (
-    <ul>
+    <ul className={styles.Quotes}>
       {quoteLis}
     </ul>
   );
--- src/services/futuramaApi.js
+++ src/services/futuramaApi.js
@@ -1,9 +1,7 @@
 export const getQuotes = (count = 10) => {
-  return fetch(`https://futuramaapi.herokuapp.com/api/quotes/${count}`)
-    .then(res => ([res.ok, res.json()]))
-    .then(([ok, quotes]) => {
-      if(!ok) throw 'Unable to fetch quotes';
-      console.log(quotes);
+  return fetch(`//futuramaapi.herokuapp.com/api/quotes/${count}`)
+    .then(res => res.json())
+    .then(quotes => {
       return quotes;
     });
 };
